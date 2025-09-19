23cadd7a730e298d3a02513e04e2b38ab80dce6c
Rodrigo Vasconcelos <47873785+rrvasconcelos@users.noreply.github.com>
2025-09-19 08:37:37 -0300

    feat: adicionado exemplo e alterado a versao do pacote para 1.0.2

---
+++ src/BrazilianDocs.Example/Program.cs
@@ -0,0 +1,13 @@
+﻿using BrazilianDocs;
+
+var cpf = Cpf.Generate();
+
+Console.WriteLine($"CPF: {cpf.Value}");
+
+Console.WriteLine($"Valido? {Cpf.IsValid(cpf.Value)}");
+
+var cnpj = Cnpj.Generate();
+
+Console.WriteLine($"CNPJ: {cnpj.Value}");
+
+Console.WriteLine($"Valido? {Cnpj.IsValid(cnpj.Value)}");
